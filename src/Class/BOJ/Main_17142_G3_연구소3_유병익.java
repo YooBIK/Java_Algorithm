d667d99bef5979963f1782e320d8892bf79ba3b6
Yoobi <quddlr96@naver.com>
2023-02-26 01:51:41 +0900

    add: BOJ17142

--- src/Class/BOJ/Main_17142_G3_연구소3_유병익.java
+++ src/Class/BOJ/Main_17142_G3_연구소3_유병익.java
@@ -12,142 +12,133 @@ import java.util.StringTokenizer;
 
 public class Main_17142_G3_연구소3_유병익 {
 
-	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer stringTokenizer;
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer stringTokenizer;
 
-	static int N, M;
-	static int NUMBER_OF_VIRUS = 0;
-	static int[][] map;
-	static int[][] visit;
+    static int N, M;
+    static int NUMBER_OF_VIRUS = 0;
+    static int[][] map;
+    static boolean[][] visit;
 
-	static int[] dirRow = { 0, 0, -1, 1 };
-	static int[] dirCol = { 1, -1, 0, 0 };
+    static int[] dirRow = {0, 0, -1, 1};
+    static int[] dirCol = {1, -1, 0, 0};
 
-	static List<int[]> virusPositions = new ArrayList<>();
-	static List<int[]> selectedViruses;
-	static List<int[]> unselectedViruses = new ArrayList<>();
-	static List<int[]> maxDistPositions = new ArrayList<>();
+    static List<int[]> virusPositions = new ArrayList<>();
+    static List<int[]> selectedViruses;
 
-	static int answer = Integer.MAX_VALUE;
+    static int EMPTY_AREA = 0;
+    static int EMPTY_AREA_TEMP = 0;
+    static int CUR_TIME;
 
-	public static void main(String[] args) throws IOException {
-		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-		N = Integer.parseInt(stringTokenizer.nextToken());
-		M = Integer.parseInt(stringTokenizer.nextToken());
+    static int answer = Integer.MAX_VALUE;
 
-		map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
-				if (map[i][j] == 2) {
-					NUMBER_OF_VIRUS++;
-					virusPositions.add(new int[] { i, j });
-				}
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        M = Integer.parseInt(stringTokenizer.nextToken());
 
-		selectedViruses = new ArrayList<>(virusPositions);
-		combination(0, 0);
-		if (answer == Integer.MAX_VALUE) {
-			System.out.println(-1);
-		} else {
-			System.out.println(answer);
-		}
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                if (map[i][j] == 2) {
+                    NUMBER_OF_VIRUS++;
+                    virusPositions.add(new int[]{i, j});
+                }
+                if (map[i][j] == 0) {
+                    EMPTY_AREA++;
+                }
+            }
+        }
+        if(EMPTY_AREA == 0){
+            System.out.println(0);
+            return;
+        }
 
-	}
 
-	private static void combination(int depth, int startPosition) {
-		if (depth == NUMBER_OF_VIRUS - M) {
-			int[][] temp = copyMap();
-			visit = new int[N][N];
+        selectedViruses = new ArrayList<>(virusPositions);
+        combination(0, 0);
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
+        }
 
-			bfs(temp);
+    }
 
-			int result = getMaxValue(temp);
+    private static void combination(int depth, int startPosition) {
+        if (depth == NUMBER_OF_VIRUS - M) {
+            int[][] temp = copyMap();
 
-			if (result != Integer.MIN_VALUE) {
-				if (check(result, temp)) {
-					result -= 1;
-				} else {
-					result -= 2;
-				}
-				answer = Math.min(answer, result);
-			}
-			return;
-		}
+            visit = new boolean[N][N];
+            bfs(temp);
 
-		for (int i = startPosition; i < NUMBER_OF_VIRUS; i++) {
-			int[] curPosition = virusPositions.get(i);
-			map[curPosition[0]][curPosition[1]] = -1;
-			unselectedViruses.add(curPosition);
-			selectedViruses.remove(curPosition);
-			combination(depth + 1, i + 1);
-			map[curPosition[0]][curPosition[1]] = 2;
-			unselectedViruses.remove(curPosition);
-			selectedViruses.add(curPosition);
-		}
+            if (CUR_TIME != -1) {
+                answer = Math.min(answer, CUR_TIME);
+            }
+            return;
+        }
 
-	}
+        for (int i = startPosition; i < NUMBER_OF_VIRUS; i++) {
+            int[] curPosition = virusPositions.get(i);
+            map[curPosition[0]][curPosition[1]] = -1;
+            selectedViruses.remove(curPosition);
+            combination(depth + 1, i + 1);
+            map[curPosition[0]][curPosition[1]] = 2;
+            selectedViruses.add(curPosition);
+        }
 
-	private static boolean check(int result, int[][] temp) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (temp[i][j] == result) {
-					if (!unselectedViruses.contains(new int[] { i, j })) {
-						return false;
-					}
-				}
-			}
-		}
-		return true;
-	}
+    }
 
-	private static int getMaxValue(int[][] temp) {
-		int result = Integer.MIN_VALUE;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (temp[i][j] == 0)
-					return Integer.MIN_VALUE;
-				result = Math.max(result, visit[i][j]);
-			}
-		}
-		return result;
-	}
 
-	private static void bfs(int[][] temp) {
-		Queue<int[]> queue = new ArrayDeque<>();
-		for (int i = 0; i < selectedViruses.size(); i++) {
-			int[] cur = selectedViruses.get(i);
-			queue.offer(cur);
-			visit[cur[0]][cur[1]]++;
-		}
-		while (!queue.isEmpty()) {
-			int[] curPos = queue.poll();
-			int curRow = curPos[0];
-			int curCol = curPos[1];
+    private static void bfs(int[][] temp) {
+        EMPTY_AREA_TEMP = EMPTY_AREA;
+        CUR_TIME = -1;
 
-			for (int i = 0; i < 4; i++) {
-				int newRow = curRow + dirRow[i];
-				int newCol = curCol + dirCol[i];
-				if (newRow >= 0 && newRow < N && newCol >= 0 && newCol < N) {
-					if (visit[newRow][newCol] == 0 && temp[newRow][newCol] != 1) {
-						visit[newRow][newCol] = visit[curRow][curCol] + 1;
-						temp[newRow][newCol] = 2;
-						queue.offer(new int[] { newRow, newCol });
+        Queue<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < selectedViruses.size(); i++) {
+            int[] cur = selectedViruses.get(i);
+            int curRow = cur[0];
+            int curCol = cur[1];
+            int time = 0;
+            queue.offer(new int[]{curRow, curCol, time});
+            visit[cur[0]][cur[1]] = true;
+        }
 
-					}
-				}
-			}
-		}
-	}
+        while (!queue.isEmpty()) {
+            int[] curPos = queue.poll();
+            int curRow = curPos[0];
+            int curCol = curPos[1];
+            int curTime = curPos[2];
+            for (int i = 0; i < 4; i++) {
+                int newRow = curRow + dirRow[i];
+                int newCol = curCol + dirCol[i];
+                if (newRow >= 0 && newRow < N && newCol >= 0 && newCol < N) {
+                    if (!visit[newRow][newCol] && temp[newRow][newCol] != 1) {
+                        visit[newRow][newCol] = true;
+                        queue.offer(new int[]{newRow, newCol, curTime + 1});
+                        if (temp[newRow][newCol] == 0) {
+                            EMPTY_AREA_TEMP--;
+                        }
 
-	private static int[][] copyMap() {
-		int[][] temp = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			temp[i] = map[i].clone();
-		}
-		return temp;
-	}
+                        if (EMPTY_AREA_TEMP == 0) {
+                            CUR_TIME = curTime + 1;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static int[][] copyMap() {
+        int[][] temp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            temp[i] = map[i].clone();
+        }
+        return temp;
+    }
 
 }
