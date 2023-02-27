fa89a6610066df0eaec4d675bfb299bdcee4c79a
YooBIK <quddlr96@naver.com>
2023-02-27 17:09:27 +0900

    add: BOJ2206 ~ing

--- src/Study/Week3/BOJ2206.java
+++ src/Study/Week3/BOJ2206.java
@@ -4,19 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * # 조건
- *  - 입력 지도의 좌상단에서 우하단으로 이동
- *  - 인접한 칸으로만 이동 가능
- *  - 1로 표시된 칸으로만 이동 가능
+ * # 조건 - 입력 지도의 좌상단에서 우하단으로 이동 - 인접한 칸으로만 이동 가능 - 1로 표시된 칸으로만 이동 가능
  * 
- * # 해결 로직 
- * 	최단 거리를 출력 -> 단순 BFS로 구현 0,0 에서 출발해서 R-1,C-1에 도착하면 BREAK!
- *  visit 배열에 방문 여부 + 거리를 입력
- *  	 방문 가능하면 이전 위치까지의 거리 +1을 저장
+ * # 해결 로직 - BFS를 수행할 때, 좌표값과 함께 벽을 부술 수 있는지 함께 저장한다. - 벽을 만났을 때, 벽을 부술 수 있다면
+ * 부수고 전진해본다. - 이를 반복하면서 최단 경로를 출력한다.
  * 
  */
 public class BOJ2206 {
@@ -25,12 +21,13 @@ public class BOJ2206 {
 	static StringTokenizer stringTokenizer;
 	static StringBuilder stringBuilder = new StringBuilder();
 
-	static int[] dirRow = { 0, 0, 1, -1 };
-	static int[] dirCol = { 1, -1, 0, 0 };
+	static int[] dirRow = { 1, 0, -1, 0 };
+	static int[] dirCol = { 0, 1, 0, -1 };
 
 	static int R, C;
 	static char[][] map;
 	static int[][] visit;
+	static int result = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
@@ -45,33 +42,43 @@ public class BOJ2206 {
 			map[i] = bufferedReader.readLine().toCharArray();
 		}
 
+		visit[0][0] = 1;
 		bfs(0, 0);
-		System.out.println(visit[R - 1][C - 1]);
+
+		if (result == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(result);
+		}
 	}
 
-	private static void bfs(int startRow, int startCol) {
+	private static void bfs(int curRow, int curCol) {
 
 		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] { startRow, startCol });
-		visit[startRow][startCol] = 1;
 
-		while (!queue.isEmpty()) {
-			int[] curPosition = queue.poll();
-			int curRow = curPosition[0];
-			int curCol = curPosition[1];
+		int count = 0;
+		for (int i = 0; i < 4; i++) {
+			int newRow = curRow + dirRow[i];
+			int newCol = curCol + dirCol[i];
+			if (newRow >= 0 && newRow < R && newCol >= 0 && newCol < C) {
+				if (visit[newRow][newCol] == 0) {
+					if (map[newRow][newCol] == '0') {
+						if (visit[newRow][newCol] == 0) {
+							visit[newRow][newCol] = visit[curRow][curCol] + 1;
+							visit[newRow][newCol] = 0;
+						} else {
+							if (visit[newRow][newCol] < visit[curRow][curCol] + 1) {
 
-			for (int i = 0; i < 4; i++) {
-				int newRow = curRow + dirRow[i];
-				int newCol = curCol + dirCol[i];
-				if (newRow >= 0 && newRow < R && newCol >= 0 && newCol < C) {
-					if (map[newRow][newCol] == '1' && visit[newRow][newCol] == 0) {
-						queue.offer(new int[] { newRow, newCol });
-						visit[newRow][newCol] = visit[curRow][curCol] + 1;
+							}
+
+						}
+
 					}
+
 				}
 			}
-
 		}
+
 	}
 
 }
