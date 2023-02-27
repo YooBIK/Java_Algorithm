6ccf90131d86434f29d7426dd6943d92bddcbb61
Yoobi <quddlr96@naver.com>
2023-02-27 22:50:38 +0900

    add: BOJ1967 주석 추가

--- src/Study/Week3/BOJ1967.java
+++ src/Study/Week3/BOJ1967.java
@@ -5,7 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
+/**
+ * List에 {자식노드 번호, 가중치 }를 담아 해당 리스트를 배열에 저장한다.
+ * 두 노드를 잡아서 늘린다는 것 -> 한 노드에서 시작해서 자식을 타고 리프까지 가는 경우들 중 2개를 고른다는 것!
+ * 즉, 리프가 아닌 (자식이 있는) 노드에서 출발해서
+ * 해당 서브트리의 리프까지 가는 길이를 모두 구하고 그 중 가장 큰 값 2개를 고른다.(자식이 하나라면 그 값이 최대값임)
+ * 리프노드가 아닌 모든 노드에 해당 작업을 수행하고 그 때 마다 가장 큰 값을 갱신해준다.
+ */
 public class BOJ1967 {
 
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -33,13 +39,11 @@ public class BOJ1967 {
             }
             tree[parentNode].add(new int[]{childNode, weight});
         }
-        int maxLength = Integer.MIN_VALUE;
-        for (int i = 1; i <= N; i++) {
-            if (tree[i] != null) {
-                search(tree[i]);
+        for (int i = 1; i <= N; i++) {  // 모든 노드를 순회
+            if (tree[i] != null) {      // 리프 노드가 아니라면
+                search(tree[i]);        // 탐색 진행
             }
         }
-
         System.out.println(result);
     }
 
@@ -47,31 +51,31 @@ public class BOJ1967 {
     private static void search(List<int[]> start) {
 
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < start.size(); i++) {
+        for (int i = 0; i < start.size(); i++) {    // 모든 자식 노드를 확인
             int[] curChild = start.get(i);
             temp = Integer.MIN_VALUE;
-            dfs(curChild, curChild[1]);
-            list.add(temp);
+            dfs(curChild, curChild[1]);             // i 번째 자식 노드에 대해 dfs 진행 (리프노드까지 가면서 가중치를 모두 더해준다.)
+            list.add(temp);                         // i 번째 자식에서 출발해 리프까지 가는데 가장 긴 길이를 list에 저장
         }
 
-        Collections.sort(list, (o1, o2) -> Integer.compare(o2, o1));
-        if (list.size() == 1) {
+        Collections.sort(list, (o1, o2) -> Integer.compare(o2, o1));    // 길이들을 내림차순으로 정렬
+        if (list.size() == 1) {                                         // 길이가 1개면 그값 자체가 현재 만들 수 있는 가장 큰 지름
             result = Math.max(result, list.get(0));
         } else {
-            result = Math.max(result, list.get(0) + list.get(1));
+            result = Math.max(result, list.get(0) + list.get(1));       // 길이가 2개 이상이면 가장 큰 2개를 더한 값이 최대 지름
         }
     }
 
     private static void dfs(int[] curNode, int curValue) {
 
         List<int[]> childList = tree[curNode[0]];
-        if (childList == null) {
-            temp = Math.max(temp, curValue);
-            return;
+        if (childList == null) {                // 리프 노드라면
+            temp = Math.max(temp, curValue);    // 현재까지 가중치 합을 갱신
+            return;                             // 종료
         }
 
 
-        for (int i = 0; i < childList.size(); i++) {
+        for (int i = 0; i < childList.size(); i++) {        //모든 자식을 순회하며 dfs 진행
             int[] nextNode = childList.get(i);
             dfs(nextNode, curValue + nextNode[1]);
         }
