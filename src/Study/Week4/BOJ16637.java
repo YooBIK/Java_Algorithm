73773cf268483bf342e356ccc9d2d07b42ec1a83
YooBIK <quddlr96@naver.com>
2023-03-03 16:05:39 +0900

    add: Week4 probs

---
+++ src/Study/Week4/BOJ16637.java
@@ -0,0 +1,7 @@
+package Study.Week4;
+
+public class BOJ16637 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/Study/Week4/BOJ17472.java
@@ -0,0 +1,7 @@
+package Study.Week4;
+
+public class BOJ17472 {
+	public static void main(String[] args) {
+
+	}
+}
