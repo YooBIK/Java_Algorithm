5fd46b5c953ecc783fd1336d7730e7f021b7b6f6
YooBIK <quddlr96@naver.com>
2023-03-06 11:17:42 +0900

    add: BOJ17136

--- src/Study/Week4/BOJ17136.java
+++ src/Study/Week4/BOJ17136.java
@@ -3,116 +3,125 @@ package Study.Week4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ17136 {
 
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer stringTokenizer;
-    static int answer = Integer.MAX_VALUE;
-    static int paperArea = 0;
-    static boolean findFlag = false;
-    static int[] paperArray = {0, 5, 5, 5, 5, 5};
-    static int[][] map = new int[10][10];
+	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer stringTokenizer;
+	static int answer = Integer.MAX_VALUE;
+	static int paperArea = 0;
+	static boolean findFlag = false;
+	static int[] paperArray = { 0, 5, 5, 5, 5, 5 };
+	static int[][] map = new int[10][10];
+	static List<Integer>[][] canDo = new ArrayList[10][10];
 
+	public static void main(String[] args) throws IOException {
+		for (int i = 0; i < 10; i++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			for (int j = 0; j < 10; j++) {
+				map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+				if (map[i][j] == 1) {
+					paperArea++;
+				}
+			}
+		}
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 10; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 0; j < 10; j++) {
-                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
-                if (map[i][j] == 1) {
-                    paperArea++;
-                }
-            }
-        }
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				canDo[i][j] = new ArrayList<>();
+				for (int k = 5; k >= 1; k--) {
+					if (canCover(i, j, k)) {
+						canDo[i][j].add(k);
+					}
+				}
+			}
+		}
 
-        dfs(0);
-        if (answer == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);
-        }
+		dfs(0,0);
+		if (answer == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(answer);
+		}
 
-    }
+	}
 
-    private static void dfs(int depth) {
+	private static void dfs(int depth, int coveredArea) {
+		
+		if(depth > answer) {
+			return;
+		}
+		
+		if (coveredArea == paperArea) {
+			answer = Math.min(answer, depth);
+			return;
+		}
 
-        if (paperArea == 0) {
-            answer = Math.min(answer, depth);
-            findFlag = true;
-            return;
-        }
+//		System.out.println(depth);
+//		System.out.println(Arrays.toString(paperArray));
+//		for (int[] arr : map) {
+//			System.out.println(Arrays.toString(arr));
+//		}
+//		System.out.println();
+//		try {
+//			Thread.sleep(500);
+//		} catch (InterruptedException e) {
+//			throw new RuntimeException(e);
+//		}
 
-//        for (int[] arr : map) {
-//            System.out.println(Arrays.toString(arr));
-//        }
-//        System.out.println();
-//        try {
-//            Thread.sleep(500);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
+		boolean flag = false;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (map[i][j] == 1) {
+					for (int size = 5; size >= 1; size--) {
+						if (canCover(i, j, size) && paperArray[size] > 0) {
+							flag = true;
+							cover(i, j, size);
+							paperArray[size]--;
+							dfs(depth + 1, coveredArea + (size * size));
+							recover(i, j, size);
+							paperArray[size]++;
+						}
+					}
+					if (flag) {
+						return;
+					}
+				}
+			}
+		}
+	}
 
-        for (int k = 5; k >= 1; k--) {
-            int temp = 0;
-            for (int l = 1; l <= k; l++) {
-                temp += paperArray[l] * l * l;
-            }
-            if (temp < paperArea) break;
-            for (int i = 0; i <= 10 - k; i++) {
-                for (int j = 0; j <= 10 - k; j++) {
-                    if (canCover(i, j, k) && paperArray[k] > 0) { //덮을 수 있으면 일단 덮는다.
-                        cover(i, j, k);
-                        paperArea -= k * k;
-                        paperArray[k]--;
-                        dfs(depth + 1);
-                        if (findFlag) {
-                            return;
-                        }
-                        paperArea += k * k;
-                        paperArray[k]++;
-                        recover(i, j, k);
-                    }
-                }
-            }
-        }
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j <= 10; j++) {
-                for (int k = 5; k >= 1; k--) {
+	private static void cover(int startRow, int startCol, int size) {
+		for (int i = startRow; i < startRow + size; i++) {
+			for (int j = startCol; j < startCol + size; j++) {
+				map[i][j] = 0;
+			}
+		}
+	}
 
-                }
-            }
-        }
-    }
+	private static void recover(int startRow, int startCol, int size) {
+		for (int i = startRow; i < startRow + size; i++) {
+			for (int j = startCol; j < startCol + size; j++) {
+				map[i][j] = 1;
+			}
+		}
+	}
 
-    private static void cover(int startRow, int startCol, int size) {
-        for (int i = startRow; i < startRow + size; i++) {
-            for (int j = startCol; j < startCol + size; j++) {
-                map[i][j] = 0;
-            }
-        }
-    }
-
-    private static void recover(int startRow, int startCol, int size) {
-        for (int i = startRow; i < startRow + size; i++) {
-            for (int j = startCol; j < startCol + size; j++) {
-                map[i][j] = 1;
-            }
-        }
-    }
-
-    private static boolean canCover(int row, int col, int size) {
-        if (row + size > 10 || col + size > 10) return false;
-        for (int i = row; i < row + size; i++) {
-            for (int j = col; j < col + size; j++) {
-                if (map[i][j] == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	private static boolean canCover(int row, int col, int size) {
+		if (row + size > 10 || col + size > 10)
+			return false;
+		for (int i = row; i < row + size; i++) {
+			for (int j = col; j < col + size; j++) {
+				if (map[i][j] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
 }
