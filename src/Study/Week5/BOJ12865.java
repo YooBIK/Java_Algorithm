c362541694c78680021165e657e18a59ef4dfd39
YooBIK <quddlr96@naver.com>
2023-03-13 17:55:51 +0900

    add: BOJ12865 ~ing

--- src/Study/Week5/BOJ12865.java
+++ src/Study/Week5/BOJ12865.java
@@ -1,4 +1,31 @@
 package Study.Week5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class BOJ12865 {
+
+	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer stringTokenizer;
+	static StringBuilder stringBuilder = new StringBuilder();
+
+	static int[] dp;
+	static int[] count;
+	static int N, K;
+	static int temp = 0;
+
+	public static void main(String[] args) throws IOException {
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		N = Integer.parseInt(stringTokenizer.nextToken());
+		K = Integer.parseInt(stringTokenizer.nextToken());
+
+	}
+
 }
