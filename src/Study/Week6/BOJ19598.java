7e9d7b94ddc29a7086ddc071bff9ece301c4075c
YooByeongIk <quddlr96@naver.com>
2023-03-19 17:50:32 +0900

    add: BOJ19598

--- src/Study/Week6/BOJ19598.java
+++ src/Study/Week6/BOJ19598.java
@@ -3,17 +3,14 @@ package Study.Week6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 /**
  * 1. 회의를 시작이간이 빠른 순서로 정렬한다.
  * 2. 회의를 배정한다.
  *  2.1 진행중인 회의가 하나도 없다면 회의실을 배정한다.
- *  2.2 진행중인 회의가 있다면 진행중인 회의 중 끝나는 시간이 시작시간보다 작은지 비교한다.
+ *  2.2 진행중인 회의가 있다면 진행중인 회의 중 끝나는 시간이 가장 빠른 시작시간과 비교한다.
  *      -> 이 떄 존재한다면, 그 회의실을 선택한다.
  *      -> 하나도 존재하지 않는다면 새로운 회의실을 배정한다.
  */
@@ -34,30 +31,19 @@ public class BOJ19598 {
         }
         Collections.sort(meetingList, (o1, o2) -> o1[0] == o2[0] ? Long.compare(o1[1], o2[1]) : Long.compare(o1[0], o2[0]));
 
-        List<Long> usingMeetingRooms = new ArrayList<>();
+        PriorityQueue<Long> usingMeetingRooms = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            long[] curMeeting = meetingList.get(i);
-            if (usingMeetingRooms.isEmpty()) {
-                usingMeetingRooms.add(curMeeting[1]);
-            } else {
-                if (!isExist(usingMeetingRooms, curMeeting)) {
-                    usingMeetingRooms.add(curMeeting[1]);
-                }
-            }
+            arange(usingMeetingRooms,meetingList.get(i));
         }
         System.out.println(usingMeetingRooms.size());
     }
 
-    private static boolean isExist(List<Long> usingMeetingRooms, long[] curMeeting) {
-        for (int i = 0; i < usingMeetingRooms.size(); i++) {
-            if (usingMeetingRooms.get(i) <= curMeeting[0]) {
-                usingMeetingRooms.remove(i);
-                usingMeetingRooms.add(curMeeting[1]);
-                return true;
-            }
+    private static void arange(PriorityQueue<Long> usingMeetingRooms, long[] curMeeting) {
+        if(!usingMeetingRooms.isEmpty() && usingMeetingRooms.peek() <= curMeeting[0]){
+            usingMeetingRooms.poll();
         }
-        return false;
+        usingMeetingRooms.add(curMeeting[1]);
     }
 
 }
