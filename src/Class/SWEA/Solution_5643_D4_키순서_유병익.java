4efa4fb72d79e295ae3282b21746748a3ded628f
Yoobi <quddlr96@naver.com>
2023-04-03 21:22:31 +0900

    add: SWEA5643

--- src/Class/SWEA/Solution_5643_D4_키순서_유병익.java
+++ src/Class/SWEA/Solution_5643_D4_키순서_유병익.java
@@ -3,70 +3,81 @@ package Class.SWEA;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution_5643_D4_키순서_유병익 {
 
-	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer stringTokenizer;
-	static StringBuilder stringBuilder = new StringBuilder();
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer stringTokenizer;
+    static StringBuilder stringBuilder = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		int T = Integer.parseInt(bufferedReader.readLine());
-		for (int testCase = 1; testCase <= T; testCase++) {
-			int N = Integer.parseInt(bufferedReader.readLine());
-			int M = Integer.parseInt(bufferedReader.readLine());
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int testCase = 1; testCase <= T; testCase++) {
+            int N = Integer.parseInt(bufferedReader.readLine());
+            int M = Integer.parseInt(bufferedReader.readLine());
 
-			boolean[][] isSmall = new boolean[N][N];
-			boolean[][] isTall = new boolean[N][N];
 
-			for (int i = 0; i < M; i++) {
-				stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-				int less = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-				int greater = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-				isSmall[less][greater] = true; // less < greater ;
-				isTall[greater][less] = true; // greater > less;
-			}
+            List<Integer>[] isSmall = new ArrayList[N];
+            List<Integer>[] isTall = new ArrayList[N];
 
-			int answer = 0;
-			for (int i = 0; i < N; i++) {
-				Queue<Integer> queue = new ArrayDeque<>();
-				queue.offer(i);
-				int count = 0;
-				while (!queue.isEmpty()) {
-					int curNode = queue.poll();
+            for (int i = 0; i < N; i++) {
+                isSmall[i] = new ArrayList<>();
+                isTall[i] = new ArrayList<>();
+            }
 
-					for (int j = 0; j < N; j++) {
-						if (isTall[j][curNode]) {
-							queue.offer(j);
-							count++;
-						}
-					}
-				}
 
-				queue.offer(i);
-				while (!queue.isEmpty()) {
-					int curNode = queue.poll();
-					for (int j = 0; j < N; j++) {
-						if (curNode == j)
-							continue;
-						if (isTall[j][curNode]) {
-							queue.offer(j);
-							count++;
-						}
-					}
-				}
+            for (int i = 0; i < M; i++) {
+                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                int less = Integer.parseInt(stringTokenizer.nextToken()) - 1;
+                int greater = Integer.parseInt(stringTokenizer.nextToken()) - 1;
 
-				if (count == N - 1) {
-					answer++;
-				}
-			}
+                isSmall[greater].add(less);
+                isTall[less].add(greater);
+            }
 
-			stringBuilder.append("#").append(testCase).append(" ").append(answer).append("\n");
-		}
-		System.out.println(stringBuilder);
-	}
+            int answer = 0;
+            for (int i = 0; i < N; i++) {
+                boolean[] check = new boolean[N];
+                Queue<Integer> queue = new ArrayDeque<>();
+                queue.offer(i);
+                check[i] = true;
+                int count = 0;
+
+                while (!queue.isEmpty()) {
+                    int curNode = queue.poll();
+                    List<Integer> curList = isSmall[curNode];
+                    for (int j = 0; j < curList.size(); j++) {
+                        if (!check[curList.get(j)]) {
+                            check[curList.get(j)] = true;
+                            queue.offer(curList.get(j));
+                            count++;
+                        }
+                    }
+                }
+
+                check = new boolean[N];
+                queue.offer(i);
+                check[i] = true;
+                while (!queue.isEmpty()) {
+                    int curNode = queue.poll();
+                    List<Integer> curList = isTall[curNode];
+                    for (int j = 0; j < curList.size(); j++) {
+                        if (!check[curList.get(j)]) {
+                            check[curList.get(j)] = true;
+                            queue.offer(curList.get(j));
+                            count++;
+                        }
+                    }
+                }
+                if (count == N - 1) {
+                    answer++;
+                }
+            }
+            stringBuilder.append("#").append(testCase).append(" ").append(answer).append("\n");
+        }
+        System.out.println(stringBuilder);
+    }
 }
+
