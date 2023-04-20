c817d2f9ede1ee3a87fe6eb41c5687d9fa3b6fc9
YooByeongIk <quddlr96@naver.com>
2023-04-20 21:25:27 +0900

    add: BOJ2293

--- src/etc/BOJ2293.java
+++ src/etc/BOJ2293.java
@@ -3,54 +3,37 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ2293 {
 
-	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer stringTokenizer;
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer stringTokenizer;
 
-	static int N, K;
-	static int[] dp;
+    static int N, K;
 
-	public static void main(String[] args) throws IOException {
-		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    public static void main(String[] args) throws IOException {
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-		N = Integer.parseInt(stringTokenizer.nextToken());
-		K = Integer.parseInt(stringTokenizer.nextToken());
-		List<Integer> coinList = new ArrayList<>();
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        K = Integer.parseInt(stringTokenizer.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			coinList.add(Integer.parseInt(bufferedReader.readLine()));
-		}
+        int[] coinArray = new int[N + 1];
+        int[] dp = new int[K + 1];
 
-		dp = new int[K + 1];
+        for (int i = 1; i <= N; i++) {
+            coinArray[i] = Integer.parseInt(bufferedReader.readLine());
+        }
 
-		for (int j = coinList.get(0); j <= K; j += coinList.get(0)) {
-			dp[j]++;
-		}
-		System.out.println(Arrays.toString(dp));
-
-		for (int i = 1; i < N; i++) {
-			int curCoin = coinList.get(i);
-
-			for (int j = curCoin; j <= K; j += curCoin) {
-				dp[j]++;
-			}
-
-			for (int j = K; j >= curCoin; j--) {
-				if (dp[j - curCoin] == 0)
-					continue;
-				dp[j] += dp[j - curCoin];
-			}
-			System.out.println(Arrays.toString(dp));
-		}
-
-		System.out.println(dp[K]);
-
-	}
-
+        for (int i = 1; i <= N; i++) {
+            int curCoin = coinArray[i];
+            if (curCoin > K) continue;
+            dp[curCoin]++;
+            for (int j = 1; j <= K; j++) {
+                if (j - curCoin > 0)
+                    dp[j] = dp[j] + dp[j - curCoin];
+            }
+        }
+        System.out.println(dp[K]);
+    }
 }
