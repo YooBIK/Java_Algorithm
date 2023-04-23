fba4a5ed3fbafcce7e4f725efceba2e9499247ab
YooByeongIk <quddlr96@naver.com>
2023-04-23 23:51:53 +0900

    add: BOJ15684

--- src/Study/Week11/BOJ15684_사다리게임.java
+++ src/Study/Week11/BOJ15684_사다리게임.java
@@ -3,10 +3,7 @@ package Study.Week11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ15684_사다리게임 {
 
@@ -27,9 +24,9 @@ public class BOJ15684_사다리게임 {
         M = Integer.parseInt(stringTokenizer.nextToken());
         H = Integer.parseInt(stringTokenizer.nextToken());
 
-        map = new boolean[H + 2][N * 2 + 1];
+        map = new boolean[H + 2][N * 2 - 1];
 
-        for (int i = 0; i <= 2 * N; i += 2) {
+        for (int i = 0; i < 2 * N - 1; i += 2) {
             for (int j = 0; j <= H + 1; j++) {
                 map[j][i] = true;
             }
@@ -37,107 +34,95 @@ public class BOJ15684_사다리게임 {
 
         for (int i = 0; i < M; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int row = (Integer.parseInt(stringTokenizer.nextToken()));
             int col = (Integer.parseInt(stringTokenizer.nextToken()) - 1) * 2 + 1;
-            int row = Integer.parseInt(stringTokenizer.nextToken());
             map[row][col] = true;
         }
 
-
-        boolean flag = false;
-        for (int i = 0; i <= 3; i++) {
-            if (dfs(1, 0, i)) {
-                System.out.println(i);
-                flag = true;
-
+        List<int[]> positionList = new ArrayList<>();
+        for (int i = 1; i <= H; i++) {
+            for (int j = 1; j < 2 * N - 1; j += 2) {
+                if (!map[i][j]) {
+                    positionList.add(new int[]{i, j});
+                }
             }
         }
-        if (!flag) {
-            System.out.println(-1);
+
+        int answer = -1;
+        for (int i = 0; i <= 3; i++) {
+            if (combination(0, 0, i, positionList)) {
+                answer = i;
+                break;
+            }
         }
+        System.out.println(answer);
+
     }
 
-    private static boolean dfs(int row, int depth, int targetDepth) {
+    private static boolean combination(int startIndex, int depth, int targetDepth, List<int[]> positionList) {
 
-        if (depth == targetDepth) {
-            System.out.println(depth);
-            printMap();
-            System.out.println(play());
+        if (targetDepth == depth) {
             return play();
         }
 
-        for (int i = 1; i <= H; i++) {
-            for (int j = 1; j <= N * 2; j += 2) {
-                if (map[i][j]) continue;
 
-                if (boundaryCheck(i, j - 2) && map[i][j - 2]) {
-                    continue;
-                }
-                if (boundaryCheck(i, j + 2) && map[i][j + 2]) {
-                    continue;
-                }
-
-                map[i][j] = true;
-                if (dfs(i,depth + 1, targetDepth)) {
-                    return true;
-                }
-                map[i][j] = false;
+        for (int i = startIndex; i < positionList.size(); i++) {
+            int[] curPosition = positionList.get(i);
+            if (boundaryCheck(curPosition[0], curPosition[1] - 2) && map[curPosition[0]][curPosition[1] - 2]) {
+                continue;
             }
+            if (boundaryCheck(curPosition[0], curPosition[1] + 2) && map[curPosition[0]][curPosition[1] + 2]) {
+                continue;
+            }
+
+            map[curPosition[0]][curPosition[1]] = true;
+            if (combination(i + 1, depth + 1, targetDepth, positionList)) {
+                return true;
+            }
+            map[curPosition[0]][curPosition[1]] = false;
         }
         return false;
-    }
-
-    private static void printMap() {
-        for (int i = 0; i < H + 2; i++) {
-            for (int j = 0; j < N * 2 + 1; j++) {
-                if (map[i][j]) {
-                    System.out.print("X ");
-                } else {
-                    System.out.print("o ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 
     private static boolean play() {
-        for (int i = 0; i <= N * 2; i += 2) {
-            boolean[][] visit = new boolean[H + 2][N * 2 + 1];
-            return bfs(i, visit);
+        for (int i = 0; i < N * 2 - 1; i += 2) {
+            if (!bfs(i)) {
+                return false;
+            }
         }
-        return false;
+        return true;
     }
 
-    private static boolean bfs(int col, boolean[][] visit) {
+    private static boolean bfs(int col) {
         Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{0, col});
-        visit[0][col] = true;
 
         while (!queue.isEmpty()) {
             int[] curInfo = queue.poll();
+
             if (curInfo[0] == H + 1) {
-                if (curInfo[1] == col) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return curInfo[1] == col;
             }
-            for (int i = 0; i < 3; i++) {
+
+            boolean flag = false;
+            for (int i = 0; i < 2; i++) {
                 int newRow = curInfo[0] + dirRow[i];
                 int newCol = curInfo[1] + dirCol[i];
 
-                if (boundaryCheck(newRow, newCol) && !visit[newRow][newCol]) {
-                    queue.offer(new int[]{newRow, newCol});
-                    visit[newRow][newCol] = true;
+                if (boundaryCheck(newRow, newCol) && map[newRow][newCol]) {
+                    queue.offer(new int[]{newRow + 1, newCol + dirCol[i]});
+                    flag = true;
                     break;
                 }
+            }
+            if (!flag) {
+                queue.offer(new int[]{curInfo[0] + 1, curInfo[1]});
             }
         }
         return false;
     }
 
-
     private static boolean boundaryCheck(int row, int col) {
-        return row >= 0 && row <= H && col >= 0 && col < N * 2;
+        return row >= 0 && row <= H + 1 && col >= 0 && col < N * 2 - 1;
     }
 }
