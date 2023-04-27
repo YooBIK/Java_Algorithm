223efbd93716cb70e7e813440b4b58b1d44014d2
YooBIK <quddlr96@naver.com>
2023-04-27 09:21:53 +0900

    add: BOJ14226

--- src/etc/BOJ14226.java
+++ src/etc/BOJ14226.java
@@ -9,44 +9,46 @@ import java.util.Queue;
 
 public class BOJ14226 {
 
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    static int N;
-    static int[] visit = new int[1001];
+	static int N;
+	static boolean[][] visit = new boolean[2001][2001];
 
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(bufferedReader.readLine());
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(bufferedReader.readLine());
 
-        int answer = Integer.MAX_VALUE;
+		int answer = Integer.MAX_VALUE;
 
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{1, 0, 0}); // 개수 , 시간
-        Arrays.fill(visit, Integer.MAX_VALUE);
-        visit[1] = 0;
-        while (!queue.isEmpty()) {
-            int[] curInfo = queue.poll();
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] { 1, 0, 0 }); // 개수 , 시간
+		visit[1][0] = true;
 
-            if (curInfo[0] == N) {
-                answer = curInfo[2];
-                break;
-            }
+		while (!queue.isEmpty()) {
+			int[] curInfo = queue.poll();
 
+			if (curInfo[0] == N) {
+				answer = Math.min(answer, curInfo[2]);
+				break;
+			}
 
-            if (curInfo[1] != 0 && curInfo[0] + curInfo[1] <= 1000 && curInfo[0] + curInfo[1] > 0 && curInfo[2] + 1 < visit[curInfo[0] + curInfo[1]]) {
-                visit[curInfo[0] + curInfo[1]] = curInfo[2] + 1;
-                queue.offer(new int[]{curInfo[0] + curInfo[1], curInfo[1], curInfo[2] + 1});
-            }
+			// 붙혀 넣기
+			if (curInfo[1] > 0 && curInfo[0] + curInfo[1] <= 2000 && !visit[curInfo[0] + curInfo[1]][curInfo[1]]) {
+				queue.offer(new int[] { curInfo[0] + curInfo[1], curInfo[1], curInfo[2] + 1 });
+				visit[curInfo[0] + curInfo[1]][curInfo[1]] = true;
+			}
 
-            if (curInfo[0] > 0)
-                queue.offer(new int[]{curInfo[0], curInfo[0], curInfo[2] + 1});
+			// 복사
+			if (curInfo[0] > 0 && !visit[curInfo[0]][curInfo[0]]) {
+				visit[curInfo[0]][curInfo[0]] = true;
+				queue.offer(new int[] { curInfo[0], curInfo[0], curInfo[2] + 1 });
+			}
 
-            if (curInfo[0] > 0 && curInfo[0] - 1 <= 1000 && curInfo[0] - 1 > 0 && curInfo[2] + 1 < visit[curInfo[0] - 1]) {
-                visit[curInfo[0] - 1] = curInfo[2] + 1;
-                queue.offer(new int[]{curInfo[0] - 1, curInfo[1], curInfo[2] + 1});
-            }
+			if (curInfo[0] - 1 > 0 && !visit[curInfo[0] - 1][curInfo[1]]) {
+				visit[curInfo[0] - 1][curInfo[1]] = true;
+				queue.offer(new int[] { curInfo[0] - 1, curInfo[1], curInfo[2] + 1 });
+			}
+		}
 
-        }
-
-        System.out.println(answer);
-    }
+		System.out.println(answer);
+	}
 }
