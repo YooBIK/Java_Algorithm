fbff048cd5d84c67b698810c5fca9f2a934b72eb
YooBIK <quddlr96@naver.com>
2023-05-02 11:41:06 +0900

    add: BOJ2885

--- src/Study/Week12/BOJ2885_초콜릿식사.java
+++ src/Study/Week12/BOJ2885_초콜릿식사.java
@@ -6,58 +6,28 @@ import java.io.InputStreamReader;
 
 public class BOJ2885_초콜릿식사 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int K = Integer.parseInt(bufferedReader.readLine());
+		int K = Integer.parseInt(bufferedReader.readLine());
 
+		int size = 1;
+		while (size < K) {
+			size *= 2;
+		}
 
-        int max = 1 << 20;
+		int curSize = size;
+		int temp = K;
+		int count = 0;
 
-
-        int firstMax = -1;
-        int temp = K;
-
-        boolean isValid = true;
-        while (true) {
-            if (temp == 1 || temp == 0) break;
-            if (temp % 2 == 1) {
-                isValid = false;
-                break;
-            }
-
-
-            temp /= 2;
-        }
-
-        if (isValid) {
-            System.out.println(K + " " + 0);
-            return;
-        }
-
-        temp = K;
-        int count = 0;
-        boolean flag = false;
-        while (temp > 0) {
-            if (temp < max) {
-                max /= 2;
-                continue;
-            }
-
-            if (!flag) {
-                firstMax = max * 2;
-                flag = true;
-            }
-
-
-            temp -= max;
-            count++;
-        }
-
-        System.out.println(firstMax + " " + count);
-
-
-    }
-
-
+		while (temp > 0) {
+			if (curSize <= temp) {
+				temp -= curSize;
+			} else {
+				curSize /= 2;
+				count++;
+			}
+		}
+		System.out.println(size + " " + count);
+	}
 }
